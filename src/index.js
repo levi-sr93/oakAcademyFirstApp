327b5b8dce8ab819cd17d06fe6ff533fb58af6bb
Levi <levi-sr@outlook.com>
2020-09-23 19:20:42 -0300

    JSX Basics

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const buttonText = {text: 'Submit'}
   return (
     <div>
       <label htmlFor="name" className="label">Enter e-mail</label>
       <input id="name" type="text"/>
       <button style={{backgroundColor: "red", color: "white", marginLeft: "10px"}}>
-        Submit
+        {buttonText.text}
       </button>
 </div>
   )
